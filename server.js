e0988fd3bb0174c704c9c887aa162a8363036edf
Grace Ho <rainczech@gmail.com>
2018-05-08 14:31:04 -0500

    server boilercode

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// Express dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const app = express();
+const PORT = 8080;
+// make public first and take priority over other routes
+app.use(express.static("public"));
+
+// body parsing
+app.use(bodyParser.urlencoded({extended: true}));
+
+// handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// SQL
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password: "",
+    database: "burgers_db"
+});
