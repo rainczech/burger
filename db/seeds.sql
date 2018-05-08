34b8f7ab99dc3bd410d4f07db6885e789b4fadda
Grace Ho <rainczech@gmail.com>
2018-05-08 14:21:17 -0500

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double bacon burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Breakfast burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", 0);
