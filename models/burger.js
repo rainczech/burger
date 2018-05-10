1b9dbadf02b9580f3f4f6ba369e618387fd193fa
Grace Ho <rainczech@gmail.com>
2018-05-10 13:19:31 -0500

    burgers.js

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,22 @@
-let orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
 // create code that will call the ORM function using specific input for the ORM
 
-
-
-module.export = ?
+let burger = {
+    all:function(callback){
+        orm.allBurgers("burgers", function(result){
+            callback(result);
+        });
+    },
+    create:function(column, values, callback){
+        orm.createOne("burgers", column, values, function(result){
+            callback(result);
+        });
+    },
+    update:function(column, condition, callback){
+        orm.updateOne("burgers", column, condtition, callback, function(result){
+            callback(result)
+        });
+    }
+};
+module.export = burger;
